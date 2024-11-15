489f22549d2832788c0aa788b2566a38d350bb4b
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-11-15 23:50:08 +1100

    Validate LockdownData.

--- common/src/main/java/house/greenhouse/bovinesandbuttercups/util/LockdownData.java
+++ common/src/main/java/house/greenhouse/bovinesandbuttercups/util/LockdownData.java
@@ -1,8 +1,9 @@
 package house.greenhouse.bovinesandbuttercups.util;
 
-import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import house.greenhouse.bovinesandbuttercups.content.effect.BovinesEffects;
 import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
@@ -14,7 +15,12 @@ import java.util.Optional;
 
 public record LockdownData(Holder<MobEffect> linkedEffect, Optional<Integer> duration) {
     public static final Codec<LockdownData> CODEC = RecordCodecBuilder.create(inst -> inst.group(
-            MobEffect.CODEC.fieldOf("linked_effect").forGetter(LockdownData::linkedEffect),
+            MobEffect.CODEC.fieldOf("linked_effect").validate(holder -> {
+                if (holder.is(BovinesEffects.LOCKDOWN)) {
+                    return DataResult.error(() -> "Cannot create a lockdown effect that locks down lockdown.");
+                }
+                return DataResult.success(holder);
+            }).forGetter(LockdownData::linkedEffect),
             Codec.INT.optionalFieldOf("duration").forGetter(LockdownData::duration)
     ).apply(inst, LockdownData::new));
     public static final Codec<List<LockdownData>> ADDITIONAL_CODEC = CODEC.listOf().optionalFieldOf("bovinesandbuttercups:lockdown_data", List.of()).codec();
