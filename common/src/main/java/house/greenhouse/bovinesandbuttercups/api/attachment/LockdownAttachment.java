682e8aeba83621d60123d18d599ec13ab44dc3de
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-12-14 04:28:05 +1100

    Swap LockdownAttachment to use a ConcurrentHashMap.

--- common/src/main/java/house/greenhouse/bovinesandbuttercups/api/attachment/LockdownAttachment.java
+++ common/src/main/java/house/greenhouse/bovinesandbuttercups/api/attachment/LockdownAttachment.java
@@ -11,8 +11,8 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.LivingEntity;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 public record LockdownAttachment(Map<Holder<MobEffect>, Integer> effects) {
@@ -22,7 +22,7 @@ public record LockdownAttachment(Map<Holder<MobEffect>, Integer> effects) {
     ).codec().xmap(LockdownAttachment::new, LockdownAttachment::effects);
 
     public LockdownAttachment(Map<Holder<MobEffect>, Integer> effects) {
-        this.effects = new HashMap<>(effects);
+        this.effects = new ConcurrentHashMap<>(effects);
     }
 
     public boolean addLockdownMobEffect(Holder<MobEffect> effect, int duration) {
