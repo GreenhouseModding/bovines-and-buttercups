9bcfcda51bdb382950076fbb08b1fb6eac00e662
MerchantPug <merchantpug@gmail.com>
2025-03-01 14:56:32 +1100

    Bump version.

--- buildSrc/src/main/kotlin/house/greenhouse/bovinesandbuttercups/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/house/greenhouse/bovinesandbuttercups/gradle/Versions.kt
@@ -1,7 +1,7 @@
 package house.greenhouse.bovinesandbuttercups.gradle
 
 object Versions {
-    const val MOD = "2.1.2"
+    const val MOD = "2.2.0"
 
     // Minecraft
     const val MINECRAFT = "1.21.1"
